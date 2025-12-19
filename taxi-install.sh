9a3b4659b8970d84e1ca7cb46fa5de2c440fc956
Boris D <b88008800@gmail.com>
2025-12-19 05:27:59 +0000

    fix: escapa todas las ocurrencias de  en proxy_set_header para evitar error de variable no definida

--- taxi-install.sh
+++ taxi-install.sh
@@ -236,7 +236,7 @@ server {
         server_name _;
         location / {
                 proxy_pass http://localhost:3000;
-                proxy_set_header Host $host;
+                proxy_set_header Host \$host;
                 proxy_set_header X-Real-IP $remote_addr;
         }
         location /admin/ {
