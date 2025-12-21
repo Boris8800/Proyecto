15233290cd7a3384a54dc023f93096e862b2689b
Boris D <b88008800@gmail.com>
2025-12-21 03:46:51 +0000

    Add quick installation script

---
+++ quick-install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm -rf /root/Proyecto && sudo git clone https://github.com/Boris8800/Proyecto.git /root/Proyecto && sudo chmod -R 755 /root/Proyecto && sudo bash /root/Proyecto/install-taxi-system.sh
